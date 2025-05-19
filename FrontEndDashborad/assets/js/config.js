79bceb3431ddf75803b558a97e09a1c525cbfd48
Mohmadkahal22 <mohmadalkahal@gmail.com>
2025-05-18 20:43:41 -0500

    .
    .

---
+++ FrontEndDashborad/assets/js/config.js
@@ -0,0 +1,11 @@
+// config.js
+window.API_CONFIG = {
+    BASE_URL: 'https://highleveltecknology.com/Ms', 
+    ENDPOINTS: {
+        LOGIN: '/api/login', 
+    },
+    DEFAULT_HEADERS: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    },
+};
